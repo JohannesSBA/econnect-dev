2c1b779b6324d2b861b57688335d9851df131eaa
Johannes Bekele <johannesseleshi@gmail.com>
2023-12-29 11:47:33 -0500

    initializing shad-cdn

--- app/(protected)/dashboard/get-started/page.tsx
+++ app/(protected)/dashboard/get-started/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="w-screen  h-[calc(100vh-5rem) flex flex-col justify-center items-center">
+      <h1>Get Started</h1>
+    </div>
+  );
 };
 
 export default page;
