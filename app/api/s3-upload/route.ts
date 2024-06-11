24c7b44d044fdd49dbde358f6f585c405d78751c
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2024-06-11 10:15:34 -0400

    feat: s3 upload and db storage of string

--- app/api/s3-upload/route.ts
+++ app/api/s3-upload/route.ts
@@ -2,27 +2,23 @@ import { PutObjectCommand, S3 } from "@aws-sdk/client-s3";
 import { getServerSession } from "next-auth";
 import { options } from "../auth/[...nextauth]/options";
 import axios from "axios";
-import { preloadFont } from "next/dist/server/app-render/entry-base";
 
 export async function POST(req: Request, res: Response) {
+  console.log("hreeeee");
   const session = await getServerSession(options);
   const body = await req.formData();
+  console.log(body);
   const image = body.get("newImage");
   const resume = body.get("newResume");
+  const ImageId = body.get("Imageid");
+  const entries = Array.from(body.entries());
+  const newPostImages = entries.filter(([key]) =>
+    key.startsWith("newPostImage")
+  );
+  console.log(newPostImages);
 
-  if (!image && !resume) {
+  if (!image && !resume && newPostImages.length === 0) {
     return new Response("File is required.", { status: 400 });
-  }
-  let file: Blob | null = null;
-  if (image) {
-    file = new Blob([image]);
-  } else {
-    file = new Blob([resume as BlobPart], { type: "application/pdf" });
-  }
-
-  let buffer: Buffer | null = null;
-  if (file instanceof Blob) {
-    buffer = Buffer.from(await file.arrayBuffer());
   }
 
   const s3 = new S3({
@@ -34,27 +30,53 @@ export async function POST(req: Request, res: Response) {
   });
 
   try {
-    const fileParams = image
-      ? {
+    if (image) {
+      const file = new Blob([image]);
+      const buffer = Buffer.from(await file.arrayBuffer());
+      const fileParams = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: `image/${session?.user.id}`,
+        ContentType: "image/jpg",
+        Body: buffer,
+      };
+
+      const command = new PutObjectCommand(fileParams);
+      await s3.send(command);
+    }
+
+    if (resume) {
+      const file = new Blob([resume as BlobPart], { type: "application/pdf" });
+      const buffer = Buffer.from(await file.arrayBuffer());
+      const fileParams = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: `resume/${session?.user.id}`,
+        ContentType: "application/pdf",
+        Body: buffer,
+      };
+
+      const command = new PutObjectCommand(fileParams);
+      await s3.send(command);
+    }
+
+    if (newPostImages.length > 0) {
+      for (let i = 0; i < newPostImages.length; i++) {
+        const file = new Blob([newPostImages[i][1]]);
+        const buffer = Buffer.from(await file.arrayBuffer());
+        const fileParams = {
           Bucket: process.env.BUCKET_NAME,
-          Key: `image/${session?.user.id}`,
+          Key: `newPostImage/${session?.user.id}/${ImageId}/${i}`,
           ContentType: "image/jpg",
-          Body: buffer || undefined,
-        }
-      : {
-          Bucket: process.env.BUCKET_NAME,
-          Key: `resume/${session?.user.id}`,
-          ContentType: "application/pdf",
-          Body: buffer || undefined,
+          Body: buffer,
         };
 
-    const command = new PutObjectCommand(fileParams);
-    await s3.send(command);
+        const command = new PutObjectCommand(fileParams);
+        await s3.send(command);
+      }
+    }
 
     return new Response("OK", { status: 200 });
   } catch (error) {
     console.log(error);
     return new Response("Error", { status: 400 });
-  } finally {
   }
 }
