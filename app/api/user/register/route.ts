502c46a6d804732703b89caaa8d42aec773688f5
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2024-10-06 13:20:05 -0400

    refactor: Refactor email sending logic in user registration route

--- app/api/user/register/route.ts
+++ app/api/user/register/route.ts
@@ -1,13 +1,38 @@
 import prisma from "@/app/lib/prisma";
 import hashPassword from "@/app/helpers/hashPass";
-import { Axios, AxiosError } from "axios";
 import nodemailer from "nodemailer";
 import jwt from "jsonwebtoken";
+import { Options } from "nodemailer/lib/mailer";
+
+async function wrappedSendMail(mailOptions: Options) {
+  return new Promise((resolve, reject) => {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465, // Use 587 if TLS is required
+      secure: true,
+      auth: {
+        user: process.env.NODDEMAILER_EMAIL as string, // Your Gmail email address
+        pass: process.env.NODEMAILER_PASS as string,
+      },
+    });
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email: ", error);
+        reject(error);
+      } else {
+        console.log("Email sent: ", info.response);
+        resolve(true);
+      }
+    });
+  });
+}
 
 export async function POST(req: Request, res: Response) {
   if (req.method !== "POST") {
     return new Response("Method not allowed", { status: 403 });
   }
+
   const body = await req.json();
 
   // Check if the user with the given email already exists
@@ -34,16 +59,6 @@ export async function POST(req: Request, res: Response) {
   // If the user doesn't exist, proceed to create a new user
   const hashedPass = await hashPassword(body.password);
   let response = "Some data";
-
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465, // Use 587 if TLS is required
-    secure: true,
-    auth: {
-      user: process.env.NODDEMAILER_EMAIL as string, // Your Gmail email address
-      pass: process.env.NODEMAILER_PASS as string,
-    },
-  });
 
   const verificationToken = jwt.sign(
     {
@@ -88,84 +103,13 @@ export async function POST(req: Request, res: Response) {
               Econnect Team
           </p>
       </main>
-
   </section>`,
   };
 
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(
-      mailOptions,
-      (error: any, info: { response: string }) => {
-        if (error) {
-          console.log(error);
-          resolve(new Response("Email not sent", { status: 500 }));
-        } else {
-          console.log("Email sent: " + info.response);
-          resolve(new Response("Email sent", { status: 200 }));
-        }
-      }
-    );
-  });
-
-  // const sgMail = require("@sendgrid/mail");
-  // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  // const msg = {
-  //   to: "johannesseleshi@gmail.com", // Change to your recipient
-  //   from: "no-reply@econnectpilot.com", // Change to your verified sender
-  //   subject: "Sending with SendGrid is Fun",
-  //   text: "and easy to do anywhere, even with Node.js",
-  //   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  // };
-  // sgMail
-  //   .send(msg)
-  //   .then(() => {
-  //     console.log("Email sent");
-  //   })
-  //   .catch((error: any) => {
-  //     console.error(error);
-  //   });
-
-  //   const sgMail = require("@sendgrid/mail");
-  //   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  //   const msg = {
-  //     to: body.email, // Change to your recipient
-  //     from: "no-reply@econnectpilot.com",
-  //     subject: "Email Verification",
-  //     text: `Please verify your email by clicking on the following link: https://main.d1lrbytgl21u6i.amplifyapp.com/api/user/verify-email?token=${verificationToken}`,
-  //     html: `<section className="max-w-2xl px-6 py-8 mx-auto bg-white dark:bg-gray-900">
-  //     <header>
-  //         <a href="#">
-  //             <img className="w-auto h-7 sm:h-8" src="" alt="">
-  //         </a>
-  //     </header>
-
-  //     <main className="mt-8">
-  //         <h2 className="text-gray-700 dark:text-gray-200">Hi ${body.firstName},</h2>
-
-  //         <p className="mt-4 leading-loose text-gray-600 dark:text-gray-300">
-  //             This code will only be valid for the next 5 minutes. If the code does not work, you can use this login verification link:
-  //         </p>
-
-  //         <a href="https://main.d1lrbytgl21u6i.amplifyapp.com/api/user/verify-email?token=${verificationToken}" className="px-6 py-2 mt-6 text-sm font-medium tracking-wider text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-  //             Verify email
-  //         </a>
-
-  //         <p className="mt-8 text-gray-600 dark:text-gray-300">
-  //             Thanks, <br>
-  //             Econnect Team
-  //         </p>
-  //     </main>
-
-  // </section>`,
-  //   };
-  //   sgMail
-  //     .send(msg)
-  //     .then(() => {
-  //       console.log("Email sent");
-  //     })
-  //     .catch((error: any) => {
-  //       console.error(error);
-  //     });
-
-  return new Response(response, { status: 200 });
+  try {
+    await wrappedSendMail(mailOptions);
+    return new Response(response, { status: 200 });
+  } catch (error) {
+    return new Response("Failed to send email", { status: 500 });
+  }
 }
