c28afc6b558ccb506019bf88a68a193e4cb4405e
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2024-10-10 19:00:08 -0400

    Feat: Protected Nav component

--- app/(protected)/components/UserPicture.tsx
+++ app/(protected)/components/UserPicture.tsx
@@ -1,26 +1,57 @@
-import React from "react";
-import { User, Link } from "@nextui-org/react";
-import { getUserContent } from "@/app/helpers/getUser";
-import { getServerSession } from "next-auth";
+"use client";
+import React, { useEffect, useState } from "react";
+import { User, Link, Skeleton } from "@nextui-org/react";
+import { getServerSession } from "next-auth/next";
 import { options } from "@/app/api/auth/[...nextauth]/options";
+import { getUserContent } from "@/app/helpers/getUser";
+import prisma from "@/app/lib/prisma";
+import axios from "axios";
 
-export default async function App() {
-  const session = await getServerSession(options);
-  const userInfo = await getUserContent(session?.user.id as string);
+interface UserInfo {
+  id: string;
+  email: string | null | undefined;
+  fullName: string;
+}
+
+export default function UserPicture() {
+  const [userInfo, setUserInfo] = useState<any | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.post("/api/user/get", {});
+      if (res.status === 200) {
+        const data = await res.data;
+        setUserInfo(data);
+      }
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Skeleton className="w-12 h-12 rounded-md flex flex-col justify-between gap-2 m-4" />
+    );
+  }
 
   return (
     <div className="flex items-center gap-4">
       <Link href={"/dashboard/profile"}>
-        <User
-          as="button"
-          avatarProps={{
-            isBordered: true,
-            src: `https://econnectbucket.s3.amazonaws.com/image/${userInfo.id}`,
-          }}
-          className="transition-transform"
-          description={userInfo.email}
-          name={userInfo.fullName}
-        />
+        {isLoading ? (
+          <Skeleton className="w-full h-24 rounded-md  " />
+        ) : (
+          <User
+            as="button"
+            avatarProps={{
+              isBordered: true,
+              src: `https://econnectbucket.s3.amazonaws.com/image/${userInfo?.id}`,
+            }}
+            className="transition-transform"
+            description={userInfo?.email}
+            name={userInfo.firstName + " " + userInfo.lastName}
+          />
+        )}
       </Link>
     </div>
   );
--- app/(protected)/listings/layout.tsx
+++ app/(protected)/listings/layout.tsx
@@ -13,6 +13,7 @@ import UserPicture from "../components/UserPicture";
 import Search from "../components/Search";
 import SignOutButton from "../components/SignOutButton";
 import axios from "axios";
+import PNav from "../components/visualComponents/PNav";
 
 interface LayoutProps {
   children: ReactNode;
@@ -41,42 +42,7 @@ const Layout: React.FC<LayoutProps> = async ({ children }) => {
     <html lang="en" className="scrollbar-thin scrollbar-webkit">
       <body className={inter.className}>
         <div className="h-screen w-screen overflow-clip font-PlusJakartaSans">
-          <div className="sticky bg-zinc-100 h-20 w-screen flex items-center px-6 gap-4 rounded-md shadow-lg backdrop-blur-md">
-            <div className="flex gap-4">
-              <Link
-                href={"/employer-dashboard"}
-                className="flex gap-4 text-blue-800"
-              >
-                <GiWaterDrop />
-                <p className="hidden md:flex font-bold text-inherit">
-                  Econnect
-                </p>
-              </Link>
-              <Link
-                href="/dashboard"
-                className="rounded-lg p-2 px-4 font-semibold text-[#6C6C6C]"
-              >
-                Dashboard
-              </Link>
-              <Link
-                href="/chat"
-                className="rounded-lg p-2 px-4 font-semibold text-[#6C6C6C]"
-              >
-                Messages
-              </Link>
-              <Link
-                href="/listings"
-                className="bg-[#4773C5]/40 rounded-lg p-2 px-4 font-semibold text-[#4773C5]"
-              >
-                Listings
-              </Link>
-            </div>
-            <div className="w-5/6 hidden md:flex gap-3 justify-end">
-              <Search />
-              <UserPicture />
-              <SignOutButton />
-            </div>
-          </div>
+          <PNav />
           <aside>{children}</aside>
           <div className="fixed bottom-0 right-0 p-8 flex flex-col gap-5"></div>
         </div>
