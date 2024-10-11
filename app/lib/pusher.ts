1822f561afd224d5960b689e6e87d48d1acd6985
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2024-10-11 15:33:07 -0400

    Refactor: Update Pusher configuration to use environment variables

--- app/lib/pusher.ts
+++ app/lib/pusher.ts
@@ -7,7 +7,7 @@ import PusherPushNotifications from "@pusher/push-notifications-web";
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.PUSHER_APP_KEY,
+  key: process.env.PUSHER_APP_KEY!,
   secret: process.env.PUSHER_APP_SECRET!,
   cluster: process.env.PUSHER_CLUSTER!,
   useTLS: true,
