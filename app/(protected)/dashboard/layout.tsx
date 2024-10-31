00eb22779028067e244e7383d5b28e901b44812f
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2024-10-30 23:32:10 -0400

    Refactor: Remove unused imports and update layout in dashboard
    - Remove unused imports in employer-dashboard/page.tsx and employer-dashboard/jobs_active/page.tsx
    - Update layout in dashboard to improve responsiveness and styling

--- app/(protected)/dashboard/layout.tsx
+++ app/(protected)/dashboard/layout.tsx
@@ -8,50 +8,52 @@ import { getUserContent } from "@/app/helpers/getUser";
 import ProtectedNav from "../components/visualComponents/ProtectedNav";
 
 interface LayoutProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Econnect",
-    description: "Dashboard for Econnect",
+  title: "Econnect",
+  description: "Dashboard for Econnect",
 };
 
 const Layout: React.FC<LayoutProps> = async ({ children }) => {
-    const session = await getServerSession(options);
-    if (!session) return;
+  const session = await getServerSession(options);
+  if (!session) return;
 
-    const userInfo = await getUserContent(session.user.id);
+  const userInfo = await getUserContent(session.user.id);
 
-    const userRole = userInfo.role as string;
+  const userRole = userInfo.role as string;
 
-    const friendsList = userInfo.friends as unknown as Friend[];
+  const friendsList = userInfo.friends as unknown as Friend[];
 
-    return (
-        <html
-            lang="en"
-            className="scrollbar-thin scrollbar-webkit h-fit bg-zinc-100"
-        >
-            <body className={inter.className}>
-                <div className="w-screen font-PlusJakartaSans">
-                    <div className="sticky top-0 w-screen z-[60]">
-                        <ProtectedNav
-                            userInfoId={userInfo.id as string}
-                            userName={
-                                ((userInfo.firstName as string) +
-                                    " " +
-                                    userInfo.lastName) as string
-                            }
-                            userEmail={userInfo.email as string}
-                        />
-                    </div>
-                    <aside className="w-full bg-zinc-100">{children}</aside>
-                    <div className="fixed bottom-0 right-0 p-8 flex flex-col gap-5"></div>
-                </div>
-            </body>
-        </html>
-    );
+  return (
+    <html
+      lang="en"
+      className="scrollbar-thin scrollbar-webkit overflow-clip h-screen bg-zinc-100"
+    >
+      <body className={inter.className}>
+        <div className="w-screen font-PlusJakartaSans">
+          <div className="sticky top-0 w-screen z-[60]">
+            <ProtectedNav
+              userInfoId={userInfo.id as string}
+              userName={
+                ((userInfo.firstName as string) +
+                  " " +
+                  userInfo.lastName) as string
+              }
+              userEmail={userInfo.email as string}
+            />
+          </div>
+          <aside className="w-full h-full overflow-clip bg-zinc-100">
+            {children}
+          </aside>
+          <div className="fixed bottom-0 right-0 p-8 flex flex-col gap-5"></div>
+        </div>
+      </body>
+    </html>
+  );
 };
 
 export default Layout;
--- app/(protected)/dashboard/my-posts/page.tsx
+++ app/(protected)/dashboard/my-posts/page.tsx
@@ -13,152 +13,144 @@ import SideInfo from "../../components/visualComponents/SideInfo";
 import Posts from "../../components/visualComponents/Posts";
 
 const Page = async () => {
-    const session = await getServerSession(options);
-    if (!session || !session.user) {
-        return redirect("/auth/signin"); // handle unauthenticated state
-    }
+  const session = await getServerSession(options);
+  if (!session || !session.user) {
+    return redirect("/auth/signin"); // handle unauthenticated state
+  }
 
-    const user = await getUserContent(session.user.id);
+  const user = await getUserContent(session.user.id);
 
-    if (!user) return null; // handle case where user is null
+  if (!user) return null; // handle case where user is null
 
-    if (user.role === "EMPLOYER") {
-        redirect("/employer-dashboard");
-    } else if (user.gotStarted === false && user.role === "EMPLOYEE") {
-        redirect("/get-started");
-    }
+  if (user.role === "EMPLOYER") {
+    redirect("/employer-dashboard");
+  } else if (user.gotStarted === false && user.role === "EMPLOYEE") {
+    redirect("/get-started");
+  }
 
-    const connections = user.friends ?? [];
-    const posts = user.posts;
-    const applications = user.jobApplications;
-    const recentApplications = applications
-        ?.sort(
-            (
-                a: { createdAt: string | number | Date },
-                b: { createdAt: string | number | Date }
-            ) =>
-                new Date(b.createdAt).getTime() -
-                new Date(a.createdAt).getTime()
-        )
-        .slice(0, 2);
+  const connections = user.friends ?? [];
+  const posts = user.posts;
+  const applications = user.jobApplications;
+  const recentApplications = applications
+    ?.sort(
+      (
+        a: { createdAt: string | number | Date },
+        b: { createdAt: string | number | Date }
+      ) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    )
+    .slice(0, 2);
 
-    return (
-        <div className="w-screen h-screen overflow-clip bg-slate-100 flex justify-center font-PlusJakartSans">
-            <div className="w-1/4 flex flex-col">
-                <div className="text-2xl text-slate-800 m-8 font-semibold">
-                    Recent Applications
-                    <div className="h-[600px] w-full flex flex-col gap-2">
-                        {recentApplications?.length === 0 && (
-                            <div className="text-sm font-semibold text-slate-400">
-                                No recent applications
-                            </div>
-                        )}
-                        {recentApplications?.map(
-                            (application: {
-                                id: React.Key | null | undefined;
-                                postedById: any;
-                                title:
-                                    | string
-                                    | number
-                                    | boolean
-                                    | React.ReactElement<
-                                          any,
-                                          | string
-                                          | React.JSXElementConstructor<any>
-                                      >
-                                    | Iterable<React.ReactNode>
-                                    | React.ReactPortal
-                                    | React.PromiseLikeOfReactNode
-                                    | null
-                                    | undefined;
-                                description:
-                                    | string
-                                    | number
-                                    | boolean
-                                    | React.ReactElement<
-                                          any,
-                                          | string
-                                          | React.JSXElementConstructor<any>
-                                      >
-                                    | Iterable<React.ReactNode>
-                                    | React.ReactPortal
-                                    | React.PromiseLikeOfReactNode
-                                    | null
-                                    | undefined;
-                            }) => (
-                                <div
-                                    key={application.id}
-                                    className="flex items-center gap-4 shadow-md rounded-md"
-                                >
-                                    <div className="flex p-6 gap-2">
-                                        <Image
-                                            width={50}
-                                            height={50}
-                                            className="rounded-full"
-                                            alt="Application Image"
-                                            src={`https://econnectbucket.s3.amazonaws.com/image/${application.postedById}`}
-                                        />
-                                        <h2 className="text-lg font-semibold">
-                                            {application.title}
-                                            <p className="text-sm text-light">
-                                                {application.description}
-                                            </p>
-                                        </h2>
-                                    </div>
-                                </div>
-                            )
-                        )}
-                    </div>
-                    <div>
-                        <Link className="flex gap-3" href="/">
-                            <FiExternalLink />
-                            <h2 className="text-sm">All Applications</h2>
-                        </Link>
-                        <Link className="flex gap-3" href="/">
-                            <FiAirplay />
-                            <h2 className="text-sm">Explore</h2>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-            <div className="w-2/4 flex flex-col h-[90%] border rounded-md">
-                <div className="flex pt-2">
-                    <User
-                        name={""} // Add the 'name' property with a value
-                        avatarProps={{
-                            isBordered: true,
-                            src: `https://econnectbucket.s3.amazonaws.com/image/${user.id}`,
-                        }}
-                        className="transition-transform ml-4 translate-x-4"
-                    />
-                    <NewPost />
-                    <h1 className="font-semibold w-full text-end p-2 pr-4 text-2xl text-slate-900">
-                        Your Activity
-                    </h1>
-                </div>
-                <Link
-                    href="/dashboard/"
-                    className="text-blue-400 font-light text-center"
+  return (
+    <div className="w-screen h-screen overflow-clip bg-slate-100 flex justify-center font-PlusJakartSans">
+      <div className="w-1/4 flex flex-col">
+        <div className="text-2xl text-slate-800 m-8 font-semibold">
+          Recent Applications
+          <div className="h-[600px] w-full flex flex-col gap-2">
+            {recentApplications?.length === 0 && (
+              <div className="text-sm font-semibold text-slate-400">
+                No recent applications
+              </div>
+            )}
+            {recentApplications?.map(
+              (application: {
+                id: React.Key | null | undefined;
+                postedById: any;
+                title:
+                  | string
+                  | number
+                  | boolean
+                  | React.ReactElement<
+                      any,
+                      string | React.JSXElementConstructor<any>
+                    >
+                  | Iterable<React.ReactNode>
+                  | React.ReactPortal
+                  | React.PromiseLikeOfReactNode
+                  | null
+                  | undefined;
+                description:
+                  | string
+                  | number
+                  | boolean
+                  | React.ReactElement<
+                      any,
+                      string | React.JSXElementConstructor<any>
+                    >
+                  | Iterable<React.ReactNode>
+                  | React.ReactPortal
+                  | React.PromiseLikeOfReactNode
+                  | null
+                  | undefined;
+              }) => (
+                <div
+                  key={application.id}
+                  className="flex items-center gap-4 shadow-md rounded-md"
                 >
-                    All Posts
-                </Link>
-                <div className="h-full bg-white rounded-md m-4 text-black">
-                    <Posts
-                        id={session.user.id}
-                        userId={session.user.id}
-                        fromPage={"my"}
+                  <div className="flex p-6 gap-2">
+                    <Image
+                      width={50}
+                      height={50}
+                      className="rounded-full"
+                      alt="Application Image"
+                      src={`https://econnectbucket.s3.amazonaws.com/image/${application.postedById}`}
                     />
+                    <h2 className="text-lg font-semibold">
+                      {application.title}
+                      <p className="text-sm text-light">
+                        {application.description}
+                      </p>
+                    </h2>
+                  </div>
                 </div>
-            </div>
-            <div className="w-[26.7%] h-[90%] flex flex-col m-2 p-2">
-                <SideInfo
-                    user={user}
-                    posts={posts}
-                    applications={applications}
-                />
-            </div>
+              )
+            )}
+          </div>
+          <div>
+            <Link className="flex gap-3" href="/">
+              <FiExternalLink />
+              <h2 className="text-sm">All Applications</h2>
+            </Link>
+            <Link className="flex gap-3" href="/">
+              <FiAirplay />
+              <h2 className="text-sm">Explore</h2>
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+      <div className="w-2/4 flex flex-col h-[90%] border rounded-md">
+        <div className="flex pt-2">
+          <User
+            name={""} // Add the 'name' property with a value
+            avatarProps={{
+              isBordered: true,
+              src: `https://econnectbucket.s3.amazonaws.com/image/${user.id}`,
+            }}
+            className="transition-transform ml-4 translate-x-4"
+          />
+          <NewPost />
+          <h1 className="font-semibold w-full text-end p-2 pr-4 text-2xl text-slate-900">
+            Your Activity
+          </h1>
+        </div>
+        <Link
+          href="/dashboard/"
+          className="text-blue-400 font-light text-center"
+        >
+          All Posts
+        </Link>
+        <div className="h-full bg-white rounded-md m-4 text-black">
+          <Posts
+            id={session.user.id}
+            userId={session.user.id}
+            fromPage={"my"}
+          />
+        </div>
+      </div>
+      <div className="w-[26.7%] h-[90%] flex flex-col m-2 p-2">
+        <SideInfo user={user} posts={posts} applications={applications} />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
